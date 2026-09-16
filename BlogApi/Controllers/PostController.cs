ab5d7a846a0ccd93f24ec0a4d57155ce97e85cf1
KissAdambat <kiss.adi2007@gmail.com>
2026-09-16 09:27:22 +0200

    Get is working

---
+++ BlogApi/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using BlogApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using BlogApi.Models.DTOs;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly string ConnectionString = "Server=localhost;Database=blog;User Id=root;Password=";
+        [HttpGet]
+        public List<Post> GetAllPost()
+        {
+            List<Post> posts = new();
+            var connector = new MySqlConnection(ConnectionString);
+            connector.Open();
+            string sql = "SELECT * FROM blogpost";
+            var cmd = new MySqlCommand(sql, connector);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var post = new Post
+                {
+                    Id = dr.GetInt32("Id"),
+                    Title = dr.GetString("Title"),
+                    Content = dr.GetString("Content"),
+                    postTime = dr.GetDateTime("postTime"),
+                    updateTime = dr.GetDateTime("updateTime"),
+                    blogId = dr.GetInt32("blogId")
+                };
+                posts.Add(post);
+            }
+            connector.Close();
+            return posts;
+        }
+    }
+}
--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ce76ec317c36faf506686280dfa9cbe1a504fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc37f932cf37b1552ca22d456e0be33b0454249e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
