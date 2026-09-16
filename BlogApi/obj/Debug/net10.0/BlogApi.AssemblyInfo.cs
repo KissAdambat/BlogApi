01c95e8a2af64ebc7988d2fced74440c4cfb504a
KissAdambat <kiss.adi2007@gmail.com>
2026-09-16 11:42:23 +0200

    getall post db done

--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce752690507aa98f663cd27a19bc3904354c1a70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e57274f40314c11ef89f331700149e84b6b3780")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
